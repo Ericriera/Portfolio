b6e9c7820e448caad1407f736d35e6254e43a743
Ericriera <ericrp2002@gmail.com>
2025-04-11 08:19:42 +0200

    build: add netlify dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,22 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [
     icon({
       iconDir: "src/assets/icons",
     }),
   ],
+
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
